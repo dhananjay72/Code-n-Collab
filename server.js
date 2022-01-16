5080ddf1f9d7068152a5252c3543a77a58582798
Dhananjay Baghdane <dhananjayvb11@gmail.com>
2022-01-16 11:01:36 +0530

    made some changes for heroku hosting

--- server.js
+++ server.js
@@ -10,10 +10,20 @@ const io = socketio(server);
  
 const port = 5000;
 
- 
-
 app.use(cors());
 app.use(bodyParser.json());
+
+
+// Heroku hosting :
+if ( process.env.NODE_ENV == "production"){
+    app.use(express.static("client/build"));
+    const path = require("path");
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
+
+
 
  
 app.use(function (req, res, next) {
